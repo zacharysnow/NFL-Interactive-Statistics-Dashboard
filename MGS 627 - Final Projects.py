9822fad9b3c82bab570800b4d26c7be99b52931c
Samuel <sammi1000@gmail.com>
2025-08-07 00:19:56 -0400

    Page layout set with NFL logo, heading and copy right statement inserted

--- MGS 627 - Final Projects.py
+++ MGS 627 - Final Projects.py
@@ -12,3 +12,60 @@ response = requests.get(url2)
 data2 = response.json()
 print(data2)
 df2 = pd.DataFrame(data2)
+
+import dash
+from dash import html
+
+# NFL logo URL
+logo_url = 'https://www.clipartmax.com/png/middle/66-667753_nfl-logo-national-football-league-logo-png.png'
+
+# Initialize Dash app
+app = dash.Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+
+    # Row with logo (left) and heading (center top)
+    html.Div([
+        # Logo aligned left
+        html.Div([
+            html.Img(src=logo_url, style={
+                'width': '125px',  # reduced size by 50%
+                'display': 'inline-block',
+                'vertical-align': 'top'
+            })
+        ], style={'width': '20%', 'display': 'inline-block'}),
+
+        # Heading aligned center
+        html.Div([
+            html.H1('NFL 2024 Analysis', style={
+                'color': 'maroon',
+                'font-weight': 'bold',
+                'font-size': '50px',
+                'margin': '0',
+                'padding-top': '15px',
+                'text-align': 'center'
+            })
+        ], style={'width': '80%', 'display': 'inline-block'})
+    ], style={'width': '100%', 'display': 'flex'}),
+
+    # Footer (bottom center)
+    html.Div([
+        html.I('Copyright © 2025 Zachary, Samuel, Meghana INC')
+    ], style={
+        'position': 'fixed',
+        'bottom': '10px',
+        'width': '100%',
+        'text-align': 'center',
+        'font-style': 'italic'
+    })
+
+], style={
+    'font-size': 15,
+    'position': 'relative',
+    'min-height': '100vh'
+})
+
+# Run app
+if __name__ == '__main__':
+    app.run(debug=False)
