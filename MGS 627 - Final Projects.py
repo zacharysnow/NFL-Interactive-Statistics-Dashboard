d802613cce7c3534a16eef9156a6f9dda0713dac
Samuel <sammi1000@gmail.com>
2025-08-07 00:32:42 -0400

    Page layout set with NFL logo, heading and copy right statement inserted

--- MGS 627 - Final Projects.py
+++ MGS 627 - Final Projects.py
@@ -1,20 +1,36 @@
 import requests
 import pandas as pd
 
+#Team statistics dataframe
 url = 'https://api.sportsdata.io/v3/nfl/scores/json/TeamSeasonStats/{2024REG}?key=b3c141ba44fc4f1ab22d3200d4f8a48f'
 response = requests.get(url)
 data = response.json()
 print(data)
 df = pd.DataFrame(data)
 
+#Player statistics dataframe
 url2 = 'https://api.sportsdata.io/v3/nfl/stats/json/PlayerSeasonStats/{2024REG}?key=b3c141ba44fc4f1ab22d3200d4f8a48f'
 response = requests.get(url2)
 data2 = response.json()
 print(data2)
 df2 = pd.DataFrame(data2)
 
+<<<<<<< HEAD
 import dash
 from dash import html
+=======
+#Quarterback and passing yards dataframe
+QB_df = df2[df2['Position'] == 'QB'][['PassingYards', 'Name', 'Team', 'Position']]
+QB_df = QB_df.sort_values(by='PassingYards', ascending=False).head(32)
+
+#Runningback and rushing yards dataframe
+RB_df = df2[df2['Position'] == 'RB'][['RushingYards', 'Name', 'Team', 'Position']]
+RB_df = RB_df.sort_values(by='RushingYards', ascending=False).head(32)
+
+#Widereceiver and receiving yards dataframe
+WR_df = df2[df2['Position'] == 'WR'][['ReceivingYards', 'Name', 'Team', 'Position']]
+WR_df = WR_df.sort_values(by='ReceivingYards', ascending=False).head(32)
+>>>>>>> origin/main
 
 # NFL logo URL
 logo_url = 'https://www.clipartmax.com/png/middle/66-667753_nfl-logo-national-football-league-logo-png.png'
