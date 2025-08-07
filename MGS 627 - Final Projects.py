e2bb879ab1b3461168dce304f3f2117275124b7f
zacharysnow <zsnow@buffalo.edu>
2025-08-07 00:16:45 -0400

    Final Project

--- MGS 627 - Final Projects.py
+++ MGS 627 - Final Projects.py
@@ -13,6 +13,15 @@ data2 = response.json()
 print(data2)
 df2 = pd.DataFrame(data2)
 
-df3 = df2[df2['Position'] == 'QB'][['PassingYards', 'Name', 'Team', 'Position']]
-df3 = df3.sort_values(by='PassingYards', ascending=False).head(32)
+#Quarterback and passing yards dataframe
+QB_df = df2[df2['Position'] == 'QB'][['PassingYards', 'Name', 'Team', 'Position']]
+QB_df = QB_df.sort_values(by='PassingYards', ascending=False).head(32)
+
+#Runningback and rushing yards dataframe
+RB_df = df2[df2['Position'] == 'RB'][['RushingYards', 'Name', 'Team', 'Position']]
+RB_df = RB_df.sort_values(by='RushingYards', ascending=False).head(32)
+
+#Widereceiver and receiving yards dataframe
+WR_df = df2[df2['Position'] == 'WR'][['ReceivingYards', 'Name', 'Team', 'Position']]
+WR_df = WR_df.sort_values(by='ReceivingYards', ascending=False).head(32)
 
