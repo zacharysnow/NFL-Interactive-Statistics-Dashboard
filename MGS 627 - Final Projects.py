1d65a4f2e13017c9e06488362c8ea9dee87d34eb
zacharysnow <zsnow@buffalo.edu>
2025-08-06 20:41:44 -0400

    Final Project

--- MGS 627 - Final Projects.py
+++ MGS 627 - Final Projects.py
@@ -7,3 +7,8 @@ data = response.json()
 print(data)
 df = pd.DataFrame(data)
 
+url2 = 'https://api.sportsdata.io/v3/nfl/stats/json/PlayerSeasonStats/{2024REG}?key=b3c141ba44fc4f1ab22d3200d4f8a48f'
+response = requests.get(url2)
+data2 = response.json()
+print(data2)
+df2 = pd.DataFrame(data2)
